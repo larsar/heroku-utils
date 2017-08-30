f460f9e5a580792d7613572c5d3f090b4b69e65b
Lars Preben Sørsdahl <lars@preben.no>
2017-08-30 08:35:18 +0200

    Specify heroku app name as argument for db url parsing

--- db_url_parser.py
+++ db_url_parser.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 
-import os, re
+import re, sys
 from urlparse import urlparse
+from subprocess import Popen, PIPE
 
-uri = os.environ['PGURL']
+if len(sys.argv) != 2:
+    print 'Usage: %s <heroku app name>' % sys.argv[0]
+    exit(-1)
+
+app = sys.argv[1]
+print('Fetching database url for: %s\n' % app)
+process = Popen(["/usr/local/bin/heroku", "config:get", "DATABASE_URL", "-a", app], stdout=PIPE)
+(output, err) = process.communicate()
+exit_code = process.wait()
+
+if exit_code != 0:
+    print(output)
+    exit(-1)
+
+uri = output
 result = urlparse(uri)
 auth, host_port = result.netloc.split('@')
 username, password = auth.split(':')
@@ -12,6 +27,6 @@ db = re.sub('/', '', result.path)
 
 print('Host:     %s' % host)
 print('Port:     %s' % port)
-print('Database: %s' % db)
+print('Database: %s' % db.rstrip())
 print('User:     %s' % username)
 print('Password: %s' % password)
